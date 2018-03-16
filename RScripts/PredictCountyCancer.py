34498843292b2cc702af041ca682669ab58bdf22
DanLogsdon <36966837+DanLogsdon@users.noreply.github.com>
2018-03-16 12:04:57 -0400

    Add files via upload

--- RScripts/PredictCountyCancer.py
+++ RScripts/PredictCountyCancer.py
@@ -13,11 +13,11 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 #%% Reading the Top 5 Effector Values From R
-file=r'C:/Users/593419/Documents/Rscripts/fromr.csv'
+file='Rscripts/fromr.csv'
 rvariables=pd.read_csv(file)
 rvariables=rvariables.drop(rvariables.columns[0], axis=1)
 
-file=r'C:/Users/593419/Documents/Rscripts/alldata.csv'
+file='Rscripts/alldata.csv'
 alldata=pd.read_csv(file)
 alldata=alldata.dropna()
 
@@ -40,6 +40,8 @@ for variable in rvariables["x"]:
     else:
         rtest[rtest.columns[indices[i] +1]]=variable
         i=i+1
-cancerval=pd.DataFrame()        
+       
 cancerval=rf2.predict(rtest[rtest.columns[1:]])
-rvariables['New_Value']=cancerval  
+cancerval=cancerval.astype("int")
+np.savetxt("cancerval.csv", cancerval, delimiter=",")
+   
